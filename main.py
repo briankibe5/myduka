c6ce93bc011683e87bc07364759821ea5e1c9cd1
Your Name <you@example.com>
2022-07-26 21:03:51 +0300

    update

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from datetime import datetime
 from flask import Flask, redirect, render_template , request, url_for
 import psycopg2
 app = Flask(__name__)
-
+# 
 
 #Connect to an existing database
-#conn = psycopg2.connect(user="postgres", password="briankibe", host="localhost", port="5432", database="postgres")
 conn = psycopg2.connect(user="sbppptuvedzdmz", password="4195135a2aa915cea64c84560ad518e8f0599765c4dbfc6811f804116d2d6971", host="ec2-54-228-32-29.eu-west-1.compute.amazonaws.com", port="5432", database="dfj3bpv8m5dmgl")
+
 #Open a cursor to perform database operations
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS products (id serial PRIMARY KEY, name VARCHAR ( 100 ) NOT NULL,buying_price NUMERIC(14, 2), selling_price NUMERIC(14, 2), stock_quantity INT DEFAULT 0);")
@@ -20,6 +20,12 @@ conn.commit()
 # home route
 @app.route('/')
 def index():
+
+   return render_template('index.html')
+
+# home route
+@app.route('/index')
+def home_page():
 
    return render_template('index.html')
 
@@ -45,6 +51,7 @@ def sales():
 def dashboard():
    # display every product vs sales
    # fetch the data using psycopg2 cur
+   
    # python query for BAR GRAPH
    cur.execute("SELECT name,sum(selling_price*quantity) as total_sales FROM products INNER JOIN sales ON products.id = sales.pid GROUP BY name;")
    items = cur.fetchall()
@@ -63,8 +70,6 @@ def dashboard():
    for i in amount:
       labels_line.append(i[0])
       data_line.append(float(i[1]))
-
-
 
    return render_template('dashboard.html',labels=labels,data = data, labels_line=labels_line, data_line=data_line)
 
